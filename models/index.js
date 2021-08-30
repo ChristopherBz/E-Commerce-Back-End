204021a888bd2f8d57a7280cfda8bdf517331da2
ChristopherBz <81110745+ChristopherBz@users.noreply.github.com>
2021-08-30 14:41:25 +1000

    adding belongstomany

--- models/index.js
+++ models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'NULL',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: 'product_tag', 
+    unique: false,
+  },
+  as: 'associated_tags'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: 'product_tag', 
+    unique: false,
+  },
+  as: 'associated_products'
+})
 
 module.exports = {
   Product,
