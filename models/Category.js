182ceb9fe74ac02c89d17b4b7db3fec6536677ed
ChristopherBz <81110745+ChristopherBz@users.noreply.github.com>
2021-08-30 18:18:35 +1000

    added comments

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,20 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement:true
+    },
+    category_name: {
+      type: DataTypes.STRING(70),
+      allowNull: false,
+      unique: true,
+      validate: {
+        is: [/^[A-Za-z\s]*$/],
+      }
+    }
   },
   {
     sequelize,
